cc3ccdd85418b627f145d4da0b2acf2a9d8a438f
Will Holcombe <william.holcombe@uconn.edu>
2018-07-03 15:14:16 -0400

    created db for noteful

---
+++ scratch/notes-queries.sql
@@ -0,0 +1,15 @@
+/*
+SELECT * FROM notes;
+SELECT * FROM notes LIMIT 5;
+*/
+SELECT * FROM notes ORDER BY title ASC; 
+/*
+SELECT * FROM notes ORDER BY id ASC;
+
+SELECT * FROM notes ORDER BY title DESC; 
+DELETE from notes WHERE id = 1001;
+*/
+/*
+INSERT INTO notes (title, content)
+VALUES ('Calypson', 'A Story about Ralph');
+*/
